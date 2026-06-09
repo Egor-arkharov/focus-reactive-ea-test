8c80d0492a25392c2a7a708ed8a4a2283b01a845
Egor Arkharov <e.arkharov@zebra-group.ru>
2026-06-09 17:40:29 +0400

    hero social links

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,45 +5,66 @@ import SocialFacebook from '@/assets/icons/social-facebook.svg?react';
 
 export const Hero = () => {
   return (
-        <section className="hero">
-          <h1 className="hero__title title">Productivity <br />conf</h1>
+    <section className="hero">
+      <h1 className="hero__title title">Productivity <br />conf</h1>
 
-          <p className="hero__date date">November 7—8, 2024 <br />online</p>
+      <p className="hero__date date">November 7—8, 2024 <br />online</p>
 
-          <button className="hero__button button">Get Tickets</button>
+      <button className="hero__button button">Get Tickets</button>
 
-          <p className='hero__lead lead'>Productivity Conference 2024&nbsp;is the essential&nbsp;AI and no-code event for performance-driven managers and entrepreneurs.</p>
+      <p className='hero__lead lead'>Productivity Conference 2024&nbsp;is the essential&nbsp;AI and no-code event for performance-driven managers and entrepreneurs.</p>
 
-          <div className="hero__social social">
-            <ul className='social__list'>
-              <li className='social__item'>
-                <SocialInstagram />
-              </li>
-              <li className='social__item'>
-                <SocialTwitter />
-              </li>
-              <li className='social__item'>
-                <SocialFacebook />
-              </li>
-            </ul>
-          </div>
+      <div className="hero__social social">
+        <ul className='social__list'>
+          <li className='social__item'>
+            <a
+              href="#"
+              target="_blank"
+              rel="noreferrer"
+              className='social__link'
+            >
+              <SocialInstagram />
+            </a>
+          </li>
+          <li className='social__item'>
+            <a
+              href="#"
+              target="_blank"
+              rel="noreferrer"
+              className='social__link'
+            >
+              <SocialTwitter />
+            </a>
+          </li>
+          <li className='social__item'>
+            <a
+              href="#"
+              target="_blank"
+              rel="noreferrer"
+              className='social__link'
+            >
+              <SocialFacebook />
+            </a>
+          </li>
+        </ul>
+      </div>
 
-          <div className="hero__indicators indicators">
-            <ul className='indicators__list'>
-              <li className="indicators__item">
-                <p className='indicators__value'>2</p>
-                <p className='indicators__text'>Days of talks</p>
-              </li>
-              <li className="indicators__item">
-                <p className='indicators__value'>20+</p>
-                <p className='indicators__text'>Speakers sharing tips</p>
-              </li>
-              <li className="indicators__item">
-                <p className='indicators__value'>5K+</p>
-                <p className='indicators__text'>Engineers attending</p>
-              </li>
-            </ul>
-          </div>
-        </section>
+      <div className="hero__indicators indicators">
+        <ul className='indicators__list'>
+          <li className="indicators__item">
+            <p className='indicators__value'>2</p>
+            <p className='indicators__text'>Days of talks</p>
+          </li>
+          <li className="indicators__item">
+            <p className='indicators__value'>20+</p>
+            <p className='indicators__text'>Speakers sharing tips</p>
+          </li>
+          <li className="indicators__item">
+            <p className='indicators__value'>5K+</p>
+            <p className='indicators__text'>Engineers attending</p>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 };
